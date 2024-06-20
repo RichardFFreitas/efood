2e9734cda6da2546c29f07f85f88c72b3b1ff566
RichardFFreitas <richardfelipelives@gmail.com>
2024-06-20 12:44:29 -0300

    fixed: image location

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,13 +8,13 @@ const Footer = () => {
         <img src="public/logo.png" alt="Logo" />
         <SocialMidia>
           <a href="https://www.instagram.com">
-            <img src="src/assets/Vetores/instagram.png" alt="Logo Instagram" />
+            <img src="/Vetores/instagram.png" alt="Logo Instagram" />
           </a>
           <a href="https://www.facebook.com">
-            <img src="src/assets/Vetores/facebook.png" alt="Logo Facebook" />
+            <img src="/Vetores/facebook.png" alt="Logo Facebook" />
           </a>
           <a href="https://twitter.com">
-            <img src="src/assets/Vetores/Twitter.png" alt="Logo Twitter" />
+            <img src="/Vetores/Twitter.png" alt="Logo Twitter" />
           </a>
         </SocialMidia>
         <Paragrafo>A efood é uma plataforma para divulgação de estabelecimentos, a responsabilidade pela entrega, qualidade dos produtos é toda do estabelecimento contratado. </Paragrafo>
